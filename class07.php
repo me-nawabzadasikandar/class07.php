39f49be8856288a1d1db468cbeb5a409c6bf0d24
It_Me.sikandar._😊😎😛 <165310265+me-nawabzadasikandar@users.noreply.github.com>
2024-06-07 11:46:30 +0500

    Add files via upload

---
+++ class07.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>CRUD</title>
+  </head>
+  <body>
+  <form action="" method="post">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email">
+
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="pass">
+  </div>
+
+  <button type="submit" class="btn btn-primary" name="insert">Insert</button>
+</form>
+
+
+        <?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "php works";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if ($conn === false) {
+    die("ERROR: Could not connect. "
+        . mysqli_connect_error());
+}
+
+$email = $_REQUEST['email'];
+$password = $_REQUEST['pass'];
+
+$insert = "INSERT INTO test VALUES ('$email', '$password')";
+
+if (mysqli_query($conn, $insert)) {
+    echo "<h3>data stored in a database successfully.</h3>";
+
+} else {
+    echo "ERROR:  Sorry $sql. "
+    . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
